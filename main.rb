d6a3b575c3fdb5e4bc4c94921116a5f55217468f
Rama Dimasatria <ramadimasatria@gmail.com>
2016-09-26 16:25:49 +0700

    basic input output based on problem test case

--- main.rb
+++ main.rb
@@ -1,9 +1,22 @@
 require_relative 'rovers.rb'
 
-boundaries = gets.chomp
-initial_state = gets.chomp
-instructions = gets.chomp
+puts "Input:"
 
-rover = Rover.new(boundaries, initial_state)
-rover.run_instructions(instructions)
-puts rover.get_state
+boundaries = gets.chomp
+
+initial_state1 = gets.chomp
+instructions1 = gets.chomp
+
+initial_state2 = gets.chomp
+instructions2 = gets.chomp
+
+puts "Output:"
+
+rover1 = Rover.new(boundaries, initial_state1)
+rover1.run_instructions(instructions1)
+puts rover1.get_state
+
+
+rover2 = Rover.new(boundaries, initial_state2)
+rover2.run_instructions(instructions2)
+puts rover2.get_state
